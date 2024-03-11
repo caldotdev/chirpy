f64665024fd0f4cd53051c10021c070295dae2f8
caldotdev <cluckenbach@protonmail.com>
2024-03-11 21:08:34 +0100

    add health check endpoint

--- main.go
+++ main.go
@@ -20,11 +20,16 @@ func corsMiddleware(next http.Handler) http.Handler {
 
 func main() {
 	mux := http.NewServeMux()
+	mux.Handle("/app/", http.StripPrefix("/app/",http.FileServer(http.Dir("."))))
+	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte("OK"))
+	})
+
 	corsMux := corsMiddleware(mux)
 
-	mux.Handle("/", http.FileServer(http.Dir(".")))
-
-	server := http.Server{
+	server := &http.Server{
 		Addr:    ":8080",
 		Handler: corsMux,
 	}
